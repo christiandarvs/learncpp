b36bb30bf61b4adf9c73f6750ee9fdb72cdfae23
christiandarvs <christiandarvin4@gmail.com>
2026-03-12 21:38:51 +0800

    add files

---
+++ README.md
@@ -0,0 +1,3 @@
+# Learn C++
+
+This repository contains code from tutorials on the website [Learn C++](https://learncpp.com/).
---
+++ chap_1/experssions.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+int main() {
+  int birth_date{3 + 3};
+  std::cout << "Birthdate: " << birth_date << "\n";
+
+  return 0;
+}
